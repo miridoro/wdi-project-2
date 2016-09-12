708bdd172206ccc68e5857a88c148a7c811bcd3a
miridoro <miriam.wodrich@gmail.com>
2016-09-12 12:38:06 +0100

    added toggleForm

--- bikes/public/js/client.js
+++ bikes/public/js/client.js
@@ -1,58 +1,154 @@
-'use strict';
+"use strict";
 
 var googleMap = {};
 
 googleMap.mapSetup = function () {
-  var canvas = document.getElementById('map-canvas');
+    var canvas = document.getElementById('map-canvas');
 
-  var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(51.506178, -0.088369),
-    mapType: google.maps.MapTypeId.ROADMAP
-  };
+    var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(51.506178, -0.088369),
+        mapType: google.maps.MapTypeId.ROADMAP,
+        styles: [{
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{
+                "saturation": "-100"
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "labels.text",
+            "stylers": [{
+                "color": "#545454"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "saturation": "-87"
+            }, {
+                "lightness": "-40"
+            }, {
+                "color": "#f9e629"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "visibility": "on"
+            }]
+        }, {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#f0f0f0"
+            }, {
+                "saturation": "-22"
+            }, {
+                "lightness": "-16"
+            }]
+        }, {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "on"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "saturation": "-52"
+            }, {
+                "lightness": "-16"
+            }, {
+                "color": "#93c4d3"
+            }]
+        }]
+    };
 
-  //this line generates the map
-  this.map = new google.maps.Map(canvas, mapOptions);
+    //this line generates the map
+    this.map = new google.maps.Map(canvas, mapOptions);
 
-  this.getBikePoints();
+    this.getBikePoints();
 };
 
 googleMap.getBikePoints = function () {
-  return $.get("http://localhost:3000/bikes").done(this.loopThroughBikes);
+    return $.get("http://localhost:3000/bikes").done(this.loopThroughBikes);
 };
 
 googleMap.loopThroughBikes = function (data) {
-  console.log(data);
-  return $.each(data, function (index, data) {
+    console.log(data);
+    return $.each(data, function (index, data) {
 
-    googleMap.createMarkerForBike(data);
-  });
+        googleMap.createMarkerForBike(data);
+    });
 };
 
 googleMap.addInfoWindowForBike = function (data, marker) {
-  var _this = this;
+    var _this = this;
 
-  //event listener on marker using google object
+    //event listener on marker using google object
 
-  google.maps.event.addListener(marker, 'click', function () {
-    if (typeof _this.infoWindow != "undefined") _this.infoWindow.close();
+    google.maps.event.addListener(marker, 'click', function () {
+        if (typeof _this.infoWindow != "undefined") _this.infoWindow.close();
 
-    _this.infoWindow = new google.maps.InfoWindow({
-      content: '<p>' + data.name + '</p>'
+        _this.infoWindow = new google.maps.InfoWindow({
+            content: "<p>Find me at: " + data.name + "<br><p>Bikes Available: " + data.NbEmptyDocks + "</p></p>"
+        });
+
+        _this.infoWindow.open(_this.map, marker);
+        _this.map.setCenter(marker.getPosition());
     });
-
-    _this.infoWindow.open(_this.map, marker);
-    _this.map.setCenter(marker.getPosition());
-  });
 };
 
 googleMap.createMarkerForBike = function (data) {
-  var latLng = new google.maps.LatLng(data.lat, data.lon);
-  var marker = new google.maps.Marker({
-    position: latLng,
-    map: this.map
-  });
-  this.addInfoWindowForBike(data, marker);
+    var latLng = new google.maps.LatLng(data.lat, data.lon);
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: this.map,
+        icon: {
+            url: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/256/Map-Marker-Marker-Outside-Chartreuse.png",
+            scaledSize: new google.maps.Size(22, 22)
+        }
+        // animation: google.maps.Animation.DROP,
+    });
+    this.addInfoWindowForBike(data, marker);
 };
 
 $(googleMap.mapSetup.bind(googleMap));
--- bikes/src/js/client.js
+++ bikes/src/js/client.js
@@ -7,7 +7,144 @@ googleMap.mapSetup = function() {
   let mapOptions = {
     zoom: 12,
     center: new google.maps.LatLng(51.506178,-0.088369),
-    mapType: google.maps.MapTypeId.ROADMAP
+    mapType: google.maps.MapTypeId.ROADMAP,
+    styles: [
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "color": "#545454"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "-87"
+            },
+            {
+                "lightness": "-40"
+            },
+            {
+                "color": "#f9e629"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f0f0f0"
+            },
+            {
+                "saturation": "-22"
+            },
+            {
+                "lightness": "-16"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "-52"
+            },
+            {
+                "lightness": "-16"
+            },
+            {
+                "color": "#93c4d3"
+            }
+        ]
+    }
+]
   };
 
   //this line generates the map
@@ -38,7 +175,7 @@ googleMap.addInfoWindowForBike = function(data, marker) {
     if(typeof this.infoWindow != "undefined") this.infoWindow.close();
 
     this.infoWindow = new google.maps.InfoWindow({
-      content:`<p>${ data.name }</p>`
+      content:`<p>Find me at: ${ data.name }<br><p>Bikes Available: ${ data.NbEmptyDocks}</p></p>`
     });
 
     this.infoWindow.open(this.map, marker);
@@ -52,6 +189,10 @@ googleMap.createMarkerForBike = function(data) {
   let marker = new google.maps.Marker({
     position: latLng,
     map: this.map,
+    icon: {
+      url: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/256/Map-Marker-Marker-Outside-Chartreuse.png",
+      scaledSize: new google.maps.Size(22, 22)
+    }
     // animation: google.maps.Animation.DROP,
   });
   this.addInfoWindowForBike(data, marker);
