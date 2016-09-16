3029dabad31bcddae52287c90eab7c6f9f7b12b8
miridoro <miriam.wodrich@gmail.com>
2016-09-16 11:17:56 +0100

    added engines to package

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGOLAB_URI || "mongodb://localhost/",
-  // db: 'mongodb://localhost/express-authentication-bikes',
+  db: process.env.MONGOLAB_URI || "mongodb://localhost/express-authentication-bikes",
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
