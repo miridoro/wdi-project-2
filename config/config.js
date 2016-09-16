e89eb1bcc009e026c8f9e0ac1e2f63f69125384c
miridoro <miriam.wodrich@gmail.com>
2016-09-16 11:22:56 +0100

    changed config file

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGOLAB_URI || "mongodb://localhost/express-authentication-bikes",
+  db: process.env.MONGODB_URI || "mongodb://localhost/express-authentication-bikes",
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
