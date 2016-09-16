0943c624e6ebfb0b226f3c96cad455f79f5b1654
miridoro <miriam.wodrich@gmail.com>
2016-09-16 10:57:10 +0100

    added mongolab to config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/express-authentication-bikes',
+  db: process.env.MONGOLAB_URI || "mongodb://localhost/",
+  // db: 'mongodb://localhost/express-authentication-bikes',
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
